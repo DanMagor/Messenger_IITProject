a2b64f6b2533ff2af46001ba4f46954e45d4f865
Herman Tarasau <quarriedstone@gmail.com>
2017-11-06 01:08:44 +0300

    Added bit inversion

--- HttpServer/HttpServer/Program.cs
+++ HttpServer/HttpServer/Program.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Newtonsoft.Json;
+using System.Collections;
 using System.Threading;
 
 namespace HttpServer
@@ -26,6 +28,7 @@ namespace HttpServer
             
             listener.Start();
             Console.WriteLine("Listening...");
+            Console.WriteLine(DateTime.Now);
 
 
             while (true)
@@ -49,7 +52,7 @@ namespace HttpServer
 
                 HttpListenerRequest request = context.Request;
                 HttpListenerResponse response = context.Response;
-                Console.WriteLine(request.ContentType);
+                
 
                 string text;
                 using (var reader = new StreamReader(request.InputStream, request.ContentEncoding))
@@ -57,9 +60,20 @@ namespace HttpServer
                     text = reader.ReadToEnd();
                 }
 
-                Console.WriteLine(text);
+                var data = JsonConvert.DeserializeObject<MyData>(text);
 
-                //string responseString = "<HTML><BODY> Request recieved! </BODY></HTML>";
+                Console.WriteLine("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+                Console.WriteLine(DateTime.Now);
+                Console.WriteLine(data.Fname);
+                Console.WriteLine("Content MimeType: " + request.ContentType);
+                Console.WriteLine("Login: " + data.login);
+                Console.WriteLine("Offset: " + data.offset);
+                Console.WriteLine("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+
+                
+                //File.WriteAllBytes(@"C:\Users\Herman\Desktop\temprep\" + data.Fname,
+                //    Convert.FromBase64String(AddNoise(Convert.FromBase64String(data.data))));
+
                 string responseString = "done";
                 byte[] buffer = Encoding.UTF8.GetBytes(responseString);
                 response.ContentLength64 = buffer.Length;
@@ -71,6 +85,95 @@ namespace HttpServer
             }
                       
             listener.Stop();
+        }
+
+        /*
+         * 
+         * Class representation of JSON object 
+         * 
+         */
+        public class MyData
+        {
+            public string data
+            {
+                get;
+                set;
+            }
+
+            public string login
+            {
+                get;
+                set;
+            }
+
+            public string offset
+            {
+                get;
+                set;
+            }
+
+            public string Fname
+            {
+                get;
+                set;
+            }
+        }
+
+        /*
+         * 
+         * Making noise in data.
+         * 
+         * noiseKoeff is percentage of noise
+         * 
+         */
+        private static string AddNoise(byte[] data)
+        {
+            const double noiseKoeff = 0.01;
+
+            var databits = new BitArray(data);
+            Random random = new Random();
+
+            for(int i = 0; i < databits.Count; i++)
+            {
+                if(random.NextDouble() <= noiseKoeff)
+                {
+                    databits.Set(i, !databits.Get(i)); // Inverting
+                }
+            }
+
+            byte[] noisyData = ToByteArray(databits);
+
+            return Convert.ToBase64String(noisyData);
+
+        }
+
+        /*
+         * 
+         * Converts BitArray to ByteArray
+         * 
+         */
+        private static byte[] ToByteArray(BitArray bits)
+        {
+            int numBytes = bits.Count / 8;
+            if (bits.Count % 8 != 0) numBytes++;
+
+            byte[] bytes = new byte[numBytes];
+            int byteIndex = 0, bitIndex = 0;
+
+            for (int i = 0; i < bits.Count; i++)
+            {
+                if (bits[i])
+                    bytes[byteIndex] |= (byte)(1 << (7 - bitIndex));
+
+                bitIndex++;
+                if (bitIndex == 8)
+                {
+                    bitIndex = 0;
+                    byteIndex++;
+                }
+            }
+
+            return bytes;
         }
 
 
