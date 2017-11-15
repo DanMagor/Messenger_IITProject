f6defc3a2a3584c6873c02617aabf96839c37e10
KN878 <kalinskiy98@gmail.com>
2017-11-15 20:47:12 +0300

    Switched from strings to BitArrays

--- hammingCode/Program.cs
+++ hammingCode/Program.cs
@@ -1,90 +1,114 @@
 ﻿using System;
+using System.Collections;
 
 namespace hammingCode
 {
     class Program
     {
+        static byte BitArrayToByteReversed(BitArray bits)
+        {
+            for (int i = 0; i < bits.Length; i++)
+                bits[i] = !bits[i];
+            byte[] bytes = new byte[1];
+            bits.CopyTo(bytes, 0);
+            return bytes[0];
+        }
+
+        static byte BitArrayToByte(BitArray bits) {
+            byte[] bytes = new byte[1];
+            bits.CopyTo(bytes, 0);
+            return bytes[0];
+        }
+
         static byte[] hammingEncode(byte[] bin) {
-            string result = "";
             byte[] encodedArr = new byte[bin.Length * 2];
             int encodeCounter = 0;
-            int sum=0;
-            string[] tmp = new string[2];
-            int[,] encodeMatrix = new int[,] { { 0,1,1,1},{ 1,0,1,1},{ 1,1,0,1},{ 1,0,0,0},
-                {0,1,0,0 },{ 0,0,1,0},{ 0,0,0,1} };
+            bool sum=false;
+            BitArray tmp;
+            BitArray result = new BitArray(8);
+            bool[,] encodeMatrix = new bool[,] { { false,true,true,true},{ true,false,true,true},{ true,true,false,true},{ true,false,false,false},
+                {false,true,false,false },{ false,false,true,false},{ false,false,false,true} };
             for(int i=0; i<bin.Length; i++) {
-                tmp[0] = Convert.ToString(bin[i], 2).PadLeft(8,'0').Substring(0,4);
-                tmp[1] = Convert.ToString(bin[i], 2).PadLeft(8, '0').Substring(4, 4);
-                result = "0";
+                tmp = new BitArray(new byte[] { bin[i]});
+                for (int j = 0; j < tmp.Length; j++) tmp[j] = !tmp[j];
+                result[7] = false;
                 for (int j = 0; j < 7; j++) {
                     for (int k = 0; k < 4; k++) {
-                        sum^= (Convert.ToInt32(tmp[0][k])-48) & encodeMatrix[j,k];
+                        sum^=  tmp[k] && encodeMatrix[j,k];
                     }
-                    result += sum;
-                    sum = 0;
+                    result[j] = sum;
+                    sum = false;
                 }
-                encodedArr[encodeCounter++] = Convert.ToByte(result,2);
-                result = "0";
+                encodedArr[encodeCounter++] = BitArrayToByte(result);
+                result[7] = false;
                 for (int j = 0; j < 7; j++)
                 {
                     for (int k = 0; k < 4; k++)
                     {
-                        sum ^= (Convert.ToInt32(tmp[1][k])-48) & encodeMatrix[j, k];
+                        sum ^=  tmp[k+4] && encodeMatrix[j, k];
                     }
-                    result += sum;
-                    sum = 0;
+                    result[j] = sum;
+                    sum = false;
                 }
-                encodedArr[encodeCounter++] = Convert.ToByte(result, 2);
+                encodedArr[encodeCounter++] = BitArrayToByte(result);
             }
             return encodedArr;
         }
 
         static byte[] hammingDecode(byte[] bin) {
-            string result = "";
             byte[] decodedArr = new byte[bin.Length / 2];
             int decodeCounter = 0;
-            char[] arr = new char[7];
-            string tmp="";
-            int sum = 0;
+            int count = 0;
+            bool sum = false;
             int[] col = new int[3];
-            int[,] decodeMatrix = new int[,] { { 1, 0, 0, 0, 1, 1, 1 }, { 0, 1, 0, 1, 0, 1, 1 }, { 0, 0, 1, 1, 1, 0, 1 } };
+            BitArray tmp;
+            BitArray result = new BitArray(8);
+            bool[,] decodeMatrix = new bool[,] { { true, false, false, false, true, true, true }, { false, true, false, true, false, true, true }, 
+                { false, false, true, true, true, false, true } };
             for (int i = 0; i < bin.Length; i++) {
-                tmp = Convert.ToString(bin[i],2).PadLeft(8,'0').Substring(1,7);
+                tmp = new BitArray(new byte[] { bin[i]});
                 for (int j = 0; j < 3; j++) {
                     for (int k = 0; k < 7; k++) {
-                        sum ^= (Convert.ToInt32(tmp[k]) - 48) & decodeMatrix[j, k];
+                        sum ^= tmp[k] && decodeMatrix[j, k];
                     }
-                    col[j] = sum;
-                    sum = 0;
+                    col[j] = Convert.ToInt32(sum);
+                    sum = false;
                 }
-                if (col[0] == 0 && col[1] == 0 && col[2] == 0) {                    
-                    result += tmp.Substring(3, 4);
-                    if (result.Length == 8)
+                if (col[0] == 0 && col[1] == 0 && col[2] == 0) {
+                    if (count == 0)
                     {
-                        decodedArr[decodeCounter++] = Convert.ToByte(result, 2);
-                        result = "";
+                        for (int j = 3; j < 7; j++) result[j - 3] = tmp[j];
+                        count++;
+                    }
+                    else
+                    {
+                        for (int j = 3; j < 7; j++) result[j + 1] = tmp[j];
+                        decodedArr[decodeCounter++] = BitArrayToByteReversed(result);
+                        count = 0;
                     }
                     continue;
                 }
                 for (int j = 0; j < 7; j++) {
-                    if (decodeMatrix[0, j] == col[0] && decodeMatrix[1, j] == col[1] && decodeMatrix[2, j] == col[2]) {
+                    if (Convert.ToInt32(decodeMatrix[0, j]) == col[0] && Convert.ToInt32(decodeMatrix[1, j]) == col[1] 
+                        && Convert.ToInt32(decodeMatrix[2, j]) == col[2]) {
                         switch (tmp[j]) {
-                            case '0':
-                                arr = tmp.ToCharArray();
-                                arr[j] = '1';
-                                tmp = new string(arr);
+                            case false:
+                                tmp[j] = true;
                                 break;
-                            case '1':
-                                arr = tmp.ToCharArray();
-                                arr[j] = '0';
-                                tmp = new string(arr);
+                            case true:
+                                tmp[j] = false;
                                 break;
-                        }                        
-                        result += tmp.Substring(3, 4);
-                        if (result.Length == 8)
+                        }
+                        if (count == 0)
                         {
-                            decodedArr[decodeCounter++] = Convert.ToByte(result, 2);
-                            result = "";
+                            for (int k = 3; k < 7; k++) result[k - 3] = tmp[k];
+                            count++;
+                        }
+                        else
+                        {
+                            for (int k = 3; k < 7; k++) result[k + 1] = tmp[k];
+                            decodedArr[decodeCounter++] = BitArrayToByteReversed(result);
+                            count = 0;
                         }
                         break;
                     }
